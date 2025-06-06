e6b7b20c3d90794e9ed5761bdf9a0e096cb48197
Gabriel Balduíno <gabrielbalduino29@gmail.com>
2025-06-05 23:11:34 -0300

    Modulo 1 e 2

---
+++ modulo1.js
@@ -0,0 +1,54 @@
+/*
+let n = "Ola";
+
+let lista = ["Ovo", "Massa", "Corante"]
+
+lista.forEach(element => {
+    console.log(element)
+})
+
+console.log(n + " Mundo!")
+if(n.toLowerCase() == "ola")
+    console.log(1)
+else
+    console.log(0)
+*/
+
+/*
+let x = 2;
+
+x++
+
+let nome = "Gabe"
+let sobrenome = "B"
+
+let nomeCompleto = `${nome} ${sobrenome} Jr`
+
+x < 1 || x < 4 ? console.log(1) : console.log(0)
+
+console.log(nomeCompleto);
+*/
+
+/*
+let x = 20
+let y = "20"
+
+x == y ? console.log(1) : console.log(0)
+x === y ? console.log(1) : console.log(0)
+
+let isMember = true
+
+let ship = isMember ? 2 : 10
+console.log(isMember ? "Membro" : "Não membro")
+console.log("frete: " + ship)
+*/
+let profissional = "Programador"
+
+switch(profissional){
+    case "Programador":
+        console.log("Programador");
+        break;
+    case "Design":
+        console.log("Design");
+        break;
+}
---
+++ modulo2.js
@@ -0,0 +1,25 @@
+/*
+function gravidade(){
+    console.log("A gravidade do planeta é: ")
+    console.log(9.8)
+
+}
+*/
+
+/*
+function somar(n1,n2){
+    let resultado = n2 + n1;
+    console.log("Resultado: " + resultado)
+}
+
+
+somar(2,3)
+*/
+
+function nomeCompleto(nome, sobrenome){
+    return `${nome} ${sobrenome}`
+}
+
+let nomecompleto = nomeCompleto("Gabe", "Da Silva")
+
+console.log(nomecompleto)
