4c81ac74c978e14d46880d64bab83d32d504ce2e
Gabriel Balduíno <gabrielbalduino29@gmail.com>
2025-06-06 16:31:54 -0300

    Modulo 1 e 2

--- modulo2.js
+++ modulo2.js
@@ -99,3 +99,189 @@ if(validacao)
 else
     console.log("Acesso negado")
 */
+
+
+
+
+//Video Arrow function a seguir
+
+/*
+const somar = (x,y) => {
+    return x + y;
+}
+console.log(somar(10,5))
+*/
+
+//const somar = (x,y) => x + y;
+
+/*const sobrenome = (sob) => {
+    return "Gabe " + sob
+}*/
+
+/*
+const sobrenome = sob => "Gabe " + sob;
+
+const sobrenome = (sob) => "Gabe " + sob;
+
+const sobrenome = (sob) => {
+    return "Gabe " + sob;
+}
+console.log(sobrenome("Jr"))
+*/
+
+/*
+let count = 0
+
+function add() {
+    count++
+}
+
+add();
+add();
+console.log(count);
+*/
+
+/*
+let count = 0
+
+function add() {
+    let count = 0 //Preferência em variável local
+    count++
+}
+
+add();
+add();
+console.log(count); //fica 0, pois mexeu apenas na local da function e não global
+*/
+
+/*
+const square = (x) =>{
+    return x * x
+}
+
+function addSquares(a,b) {
+    return square(a) + square(b)
+}
+*/
+//Ou
+/*function addSquares(a,b) {
+    const square = (x) =>{ //uso de arrow function(const) para servir como function dentro de outra function, é uma ideia melhor
+        return x * x
+    }
+
+    return square(a) + square(b)
+}
+
+console.log(addSquares(10,5))
+*/
+
+/*
+let array = [1,2,3,4,5]
+
+console.log(array.length)
+
+array.forEach(element => {
+    if(element == 5 || element == 1)
+        console.log(element)
+});
+
+let nomes = ["gabe", "mika"]
+let lista = ["blabla", nomes]
+let lista2 = 
+[
+    "bla", 
+    [
+        "xx", 
+        "yy"
+    ]
+]
+
+console.log(lista2[1][0])//acessa "xx"
+//ou
+let achar = lista2[1]
+console.log(achar[0])
+*/
+
+/*
+let ingredientes = 
+[
+    "agua", 
+    "farinha", 
+    "ovo", 
+    "leite"
+]
+
+ingredientes.push("açucar") //add ao array
+ingredientes.pop() //retira o ultimo item do array
+ingredientes.shift() //retira o primeiro item
+
+console.log(`Total de ingredientes: ${ingredientes.length}`)
+ingredientes.forEach(element => {
+    console.log(element)
+});
+*/
+
+//Ex 7
+/*
+let carros = ["BMW", "Ferrari", "Mercedes"]
+
+const exibiCarros = () =>
+{
+    carros.forEach(element => {
+        console.log(element)
+    });
+    console.log("---------------")
+}
+exibiCarros()
+
+let posicao = carros[1]
+console.log(`Posição 2: ${posicao}`)
+console.log("---------------")
+
+carros[1] = "Audi"
+exibiCarros()
+
+carros.push("Volvo")
+exibiCarros()
+
+console.log(`Há um total de ${carros.length} veículos na lista!`)
+*/
+
+/*
+let personagem = 
+{
+    nome: "Gabe",
+    idade: 19,
+    pais: "brazil",
+    forca: 5,
+    olhos: ["preto", "azul"],
+    endereco: 
+    {
+        bairro: "Barreiro"
+    }
+}
+
+console.log(`${personagem.nome} tem ${personagem.idade} anos!`)
+console.log(personagem.endereco.bairro)
+console.log(personagem.olhos[1])
+
+personagem.forca = 10
+console.log(personagem.forca)
+personagem.olhos[0] = "verde"
+personagem.olhos.push("castanho")
+console.log(personagem.olhos)
+*/
+
+let personagem = 
+{
+    nome: "Gabe",
+    idade: 19,
+    carros: 
+    [
+        {modelo: "Mitsubishi", color: "Branco"},
+        {modelo: "Chevrolet", color: "Preto"}
+    ]
+}
+
+console.log(personagem.carros[0].modelo)
+console.log(personagem)
